545f2e4f4c3fa913ad783c2f3eef047ca52f294c
andreysemichev <andreysemichev@icloud.com>
2021-01-13 16:11:06 +0400

    Update server.ts

--- express-typescript/src/server.ts
+++ express-typescript/src/server.ts
@@ -9,7 +9,7 @@ import routes from "./routes";
 const NAMESPACE = "Server";
 const app = express();
 
-/** Using the handlebars */
+// Шаблонизатор
 const hbs = handlebars.create({
   defaultLayout: "main",
   extname: "hbs"
@@ -19,10 +19,10 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "../views"));
 
-/** Using the public directory */
+// Публичная директория
 app.use("/", express.static(path.join(__dirname, "../assets")));
 
-/** Logging the request and default locals */
+// Промежуточный обработчик
 app.use((req, res, next) => {
   logging.info(`METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`, NAMESPACE);
 
@@ -31,7 +31,6 @@ app.use((req, res, next) => {
   });
 
   const locals = res.locals;
-
   locals.title = config.server.projectName;
   locals.styles = ["/styles/main.css"];
   locals.scripts = ["/scripts/main.js"];
@@ -39,27 +38,14 @@ app.use((req, res, next) => {
   next();
 });
 
-/** Parse the body of the request */
+// Парсер форм
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-/** Rules of our API */
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-
-  if (req.method == "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-
-  next();
-});
-
-/** Routes */
+// Роуты
 app.use("/", routes.root);
 
-/** Error handling */
+// 404
 app.use((req, res, next) => {
   const error = new Error("Not found");
 
@@ -68,7 +54,7 @@ app.use((req, res, next) => {
   });
 });
 
-/** Create the server */
+// Прослушка порта
 app.listen(config.server.port, () => {
   logging.info(NAMESPACE, `Server is running ${config.server.url}:${config.server.port}`);
 });
